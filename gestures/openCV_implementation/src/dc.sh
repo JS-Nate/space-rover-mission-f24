375bb6a0ad4f9493f84278a1bf91ecc928fae789
Nathanael 100783830 <nathanael.selvaraj@ontariotechu.net>
2025-02-07 02:12:50 -0500

    fic

--- gestures/openCV_implementation/src/dc.sh
+++
@@ -1,1 +0,0 @@
-docker run --network services_default --device /dev/video0 --name gesture_container gesture
--- gestures/openCV_implementation/src/k8s.sh
+++
@@ -1,2 +0,0 @@
-docker run --rm --network kind --device /dev/video0 --name gesture_container -it gesture
-# docker run --rm --network kind --device /dev/video0 --name gesture_container gesture 
